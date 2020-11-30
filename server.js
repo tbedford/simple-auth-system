b2eb32a19c0b6f38db1ef1142425a19fa9c7a12a
Tony Bedford <tony.bedford@live.co.uk>
2020-11-30 12:19:52 +0000

    WIP

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 app.use(express.static("public"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 require("dotenv").config({
   path: __dirname + "/.env",
@@ -12,26 +16,28 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "testuser",
   host: "localhost",
-  database: "testdb",
+  database: "userdb",
   password: process.env.PASSWORD,
   port: 5432,
 });
 
 // create table
-const tablename = "users";
-
-pool.query(
-  `CREATE TABLE IF NOT EXISTS ${tablename} (id serial primary key, username varchar(255), password varchar(255), session_id CHAR (40));`,
-  (error, results) => {
-    if (error) {
-      throw error;
+function createTable(tablename) {
+  pool.query(
+    `CREATE TABLE IF NOT EXISTS ${tablename} (id serial primary key, username varchar(255), password varchar(255), session_id CHAR (40));`,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log(JSON.stringify(results.command));
     }
-    console.log(JSON.stringify(results.command));
-  }
-);
+  );
+}
+
+createTable("users");
 
 // register a user
-function addUser(uname, pword) {
+function addUser(tablename, uname, pword) {
   pool.query(
     `INSERT INTO ${tablename} (username, password) VALUES ($1, $2)`,
     [uname, pword],
@@ -45,9 +51,22 @@ function addUser(uname, pword) {
 }
 
 // get specific user
+function getUser(tablename, uname) {
+  pool.query(
+    `SELECT * FROM ${tablename} WHERE username = $1`,
+    [uname],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      return results.rows;
+    }
+  );
+}
 
-function getUser() {
-  pool.query("SELECT * FROM users WHERE id = $1", [1], (error, results) => {
+// get all users
+function getAllUsers(tablename) {
+  pool.query(`SELECT * FROM ${tablename} ORDER BY id ASC`, (error, results) => {
     if (error) {
       throw error;
     }
@@ -59,13 +78,24 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/login", (req, res) => {});
+app.post("/login", (req, res) => {
+  console.log("/login");
+  var result = getUser("users", req.body.username);
+  console.log(`User password: ${result}`)
+});
 
-app.get("/register", (req, res) => {
-  res.sendFile(__dirname + "/views/register.html");
+app.get("/register-form", (req, res) => {
+  console.log("/register-form");
+  res.sendFile(__dirname + "/views/register-form.html");
+});
+
+app.post("/register", (req, res) => {
+  console.log(`/register => ${req.body.username} ${req.body.password}`);
+  addUser("users", req.body.username, req.body.password);
 });
 
 app.get("/protected", (req, res) => {
+  console.log("/protected");
   res.sendFile(__dirname + "/views/protected.html");
 });
 
--- testdb.js
+++ testdb.js
@@ -5,26 +5,27 @@ const pool = new Pool({
   user: "testuser",
   host: "localhost",
   database: "testdb",
-  password: "password",
+  password: process.env.PASSWORD,
   port: 5432,
 });
 
 // create table
-const tablename = "anothertable"
-pool.query(
-  `CREATE TABLE IF NOT EXISTS ${tablename} (id serial primary key, username varchar(255), password varchar(255));`,
-  (error, results) => {
-    if (error) {
-      throw error;
-    }
-    console.log(JSON.stringify(results.command));
-  }
-);
-
-// add a user
-function addUser(uname, pword) {
+function createTable(tablename) {
   pool.query(
-    "INSERT INTO funkytable (username, password) VALUES ($1, $2)",
+    `CREATE TABLE IF NOT EXISTS ${tablename} (id serial primary key, username varchar(255), password varchar(255), session_id CHAR (40));`,
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      console.log(JSON.stringify(results.command));
+    }
+  );
+}
+
+// register a user
+function addUser(tablename, uname, pword) {
+  pool.query(
+    `INSERT INTO ${tablename} (username, password) VALUES ($1, $2)`,
     [uname, pword],
     (error, results) => {
       if (error) {
@@ -35,20 +36,39 @@ function addUser(uname, pword) {
   );
 }
 
-//addUser("tony", "secretWord")
+// get specific user
+function getUser(tablename, uname) {
+  pool.query(
+    `SELECT * FROM ${tablename} WHERE username = $1`,
+    [uname],
+    (error, results) => {
+      if (error) {
+        throw error;
+      }
+      return results.rows;
+    }
+  );
+}
 
 // get all users
-pool.query("SELECT * FROM funkytable ORDER BY id ASC", (error, results) => {
-  if (error) {
-    throw error;
-  }
-  console.log(JSON.stringify(results.rows));
-});
+function getAllUsers(tablename) {
+  pool.query(`SELECT * FROM ${tablename} ORDER BY id ASC`, (error, results) => {
+    if (error) {
+      throw error;
+    }
+    console.log(JSON.stringify(results.rows));
+  });
+}
 
-// get specific user
-pool.query("SELECT * FROM users WHERE id = $1", [1], (error, results) => {
-  if (error) {
-    throw error;
-  }
-  console.log(JSON.stringify(results.rows));
-});
+//createTable("users");
+//addUser("users", "fredbloggs", "secret1");
+//addUser("users", "jimsmith", "secret2");
+
+//getAllUsers("users")
+
+async function main(){
+  var res = await getUser("users", "jimsmith");
+  console.log(res)  
+}
+
+main();
